1f7481909012a6b6c1eeff2321a033f29f2ba036
Barnaby Hall <barnaby-hall@outlook.com>
2023-09-19 20:25:56 +0100

    converted to modules

--- Range Grid/rangeCell.js
+++ Range Grid/rangeCell.js
@@ -1,6 +1,4 @@
-console.log("Grid Cell")
-
-export class GridCell{
+export class ComboCell{
     constructor(parentContainer, cellName, cellPercentages) {
         this.parentContainer = parentContainer;
         this.name = cellName;
@@ -73,7 +71,7 @@ export class GridCell{
     setCellPercentages() {
         let raise = this.percentages.raise ?? 0;
         let call = this.percentages.call ?? 0;
-        let fold = this.percentages.fold ?? 0;
+        let fold = 100 - raise - call;
         
 
         let text = `${raise}:${call}:${fold}`;
@@ -93,72 +91,45 @@ export class GridCell{
         // adjust the coulors of the cells with specific actions
         let raise = this.percentages.raise ?? 0;
         let call = this.percentages.call ?? 0;
-        let fold = this.percentages.fold ?? 0;
+        let fold = this.percentages.fold ?? 100;
 
         let backgroundCSS = ""
 
         if (raise === 0 && call === 0 && fold === 0) {
-            backgroundCSS = ""
+            this.cellElements["container"].style.background = backgroundCSS;
         } else {
             backgroundCSS = `linear-gradient(to right, 
-                #941414 0%, #941414 ${raise}%, 
+                #941414 0%, #941414 ${raise}%,
                 #1f9414 ${raise}%, #1f9414 ${raise + call}%,
                 #143c94 ${raise + call}%, #143c94 100%)`
             
             this.cellElements["container"].style.background = backgroundCSS;
-        }
-                
+        }       
     }
 
     increasePercentage(action, increment) {
         this.percentages[action] += increment;
-        console.log(this.percentages)
+
+        let raise = this.percentages.raise ?? 0;
+        let call = this.percentages.call ?? 0;
+
+        if (raise + call > 100) {this.percentages[action] = 100}
+        console.log(this.name, this.percentages)
 
         this.setCellPercentages();
         this.setBackgroundCol()
     }
-}
 
-let gridContainer = document.getElementById("rangGridContainer")
+    decreasePercentage(action, increment) {
+        this.percentages[action] -= increment;
 
-const COMBOS = [	
-    ['AA' , 'AKs', 'AQs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'A6s', 'A5s', 'A4s', 'A3s', 'A2s',],
-    ['AKo', 'KK' , 'KQs', 'KJs', 'KTs', 'K9s', 'K8s', 'K7s', 'K6s', 'K5s', 'K4s', 'K3s', 'K2s',],
-    ['AQo', 'KQo', 'QQ' , 'QJs', 'QTs', 'Q9s', 'Q8s', 'Q7s', 'Q6s', 'Q5s', 'Q4s', 'Q3s', 'Q2s',],
-    ['AJo', 'KJo', 'QJo', 'JJ' , 'JTs', 'J9s', 'J8s', 'J7s', 'J6s', 'J5s', 'J4s', 'J3s', 'J2s',],
-    ['ATo', 'KTo', 'QTo', 'JTo', 'TT' , 'T9s', 'T8s', 'T7s', 'T6s', 'T5s', 'T4s', 'T3s', 'T2s',],
-    ['A9o', 'K9o', 'Q9o', 'J9o', 'T9o', '99' , '98s', '97s', '96s', '95s', '94s', '93s', '92s',],
-    ['A8o', 'K8o', 'Q8o', 'J8o', 'T8o', '98o', '88' , '87s', '86s', '85s', '84s', '83s', '82s',],
-    ['A7o', 'K7o', 'Q7o', 'J7o', 'T7o', '97o', '87o', '77' , '76s', '75s', '74s', '73s', '72s',],
-    ['A6o', 'K6o', 'Q6o', 'J6o', 'T6o', '96o', '86o', '76o', '66' , '65s', '64s', '63s', '62s',],
-    ['A5o', 'K5o', 'Q5o', 'J5o', 'T5o', '95o', '85o', '75o', '65o', '55' , '54s', '53s', '52s',],
-    ['A4o', 'K4o', 'Q4o', 'J4o', 'T4o', '94o', '84o', '74o', '64o', '54o', '44' , '43s', '42s',],
-    ['A3o', 'K3o', 'Q3o', 'J3o', 'T3o', '93o', '83o', '73o', '63o', '53o', '43o', '33' , '32s',],
-    ['A2o', 'K2o', 'Q2o', 'J2o', 'T2o', '92o', '82o', '72o', '62o', '52o', '42o', '32o', '22' ,], 
-  ]
+        let raise = this.percentages.raise ?? 0;
+        let call = this.percentages.call ?? 0;
 
-let cells = []
+        if (raise + call < 0) {this.percentages[action] = 0}
+        console.log(this.percentages)
 
-for (let x in COMBOS) {
-    for (let y in COMBOS[x]) {
-        let cell = new GridCell(gridContainer, COMBOS[x][y], {})
-        cells.push(cell)
-    }
-}
-
-document.addEventListener("keydown", (event) => {
-    console.log(event)
-    if (event.key === "ArrowRight") {
-        console.log("arrow up")
-        increasePercentage()
-    }
-})
-
-function increasePercentage() {
-    for (let i in cells) {
-        let cell = cells[i]
-        if (cell.selected) {
-            cell.increasePercentage("raise", 1)
-        }
+        this.setCellPercentages();
+        this.setBackgroundCol()
     }
 }
---
+++ Range Grid/rangeDisplayMain.js
@@ -0,0 +1,6 @@
+import { Grid } from "./RangeGrid.js";
+
+let RFI_100BB_LJ = {"Name":"RFI - 100BB - LJ", "22":{"raise":17}, "33":{"raise":17}, "44":{"raise":19}, "55":{"raise":72}, "66":{"raise":100}, "77":{"raise":100}, "88":{"raise":100}, "99":{"raise":100}, "AA":{"raise":100}, "AKs":{"raise":100}, "AQs":{"raise":100}, "AJs":{"raise":100}, "ATs":{"raise":100}, "A9s":{"raise":100}, "A8s":{"raise":100}, "A7s":{"raise":100}, "A6s":{"raise":100}, "A5s":{"raise":100}, "A4s":{"raise":100}, "A3s":{"raise":100}, "A2s":{"raise":100}, "AKo":{"raise":100}, "KK":{"raise":100}, "KQs":{"raise":100}, "KJs":{"raise":100}, "KTs":{"raise":100}, "K9s":{"raise":100}, "K8s":{"raise":100}, "K7s":{"raise":100}, "K6s":{"raise":100}, "K5s":{"raise":83}, "AQo":{"raise":100}, "KQo":{"raise":100}, "QQ":{"raise":100}, "QJs":{"raise":100}, "QTs":{"raise":100}, "Q9s":{"raise":100}, "Q8s":{"raise":91}, "AJo":{"raise":100}, "KJo":{"raise":100}, "QJo":{"raise":100}, "JJ":{"raise":100}, "JTs":{"raise":100}, "J9s":{"raise":79}, "ATo":{"raise":100}, "KTo":{"raise":24}, "QTo":{"raise":24}, "TT":{"raise":100}, "T9s":{"raise":91}, "T8s":{"raise":2}, "A9o":{"raise":12}, "98s":{"raise":15}, "87s":{"raise":16}, "76s":{"raise":19}, "65s":{"raise":24}, "54s":{"raise":17}};
+
+let gridContainer = document.getElementById("rangGridContainer")
+let rangeGrid = new Grid(gridContainer, RFI_100BB_LJ)
